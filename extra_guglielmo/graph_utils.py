fe86d4ef721cb3b09489fc65d7583faf3d4c838b
Guglielmo Felici <guglielmo.felici@protonmail.com>
2021-04-19 21:28:26 +0200

    finding paths works

---
+++ extra_guglielmo/graph_utils.py
@@ -0,0 +1,76 @@
+import networkx as nx
+
+
+def _all_simple_paths_graph(G: nx.Graph, source, target):
+    print("node is: ", source)
+    current_path = [source]
+    # visited = dict.fromkeys(G.nodes, False)
+    stack = list(G[source])
+    while stack:
+        node = stack.pop()
+        if node == None:
+            current_path.pop()
+            continue
+        elif node == target:
+            yield current_path + [node]
+            pass
+        if node in current_path:
+            continue
+        current_path += [node]
+        # elif node in current_path:
+        #     current_path.pop()
+        if G[node]:
+            stack.append(None)
+            stack += list(G[node])
+        else:
+            current_path.pop()
+
+
+def BAK_all_simple_paths_graph(G, source, target, cutoff):
+    """
+    Versione modificata di nx._all_simple_paths_graph per tornare simple paths che possano partire e arrivare allo stesso nodo
+    """
+    print(source, "\n")
+    visited = []  # [source]
+    stack = [iter(G[source])]
+    while stack:
+        children = stack[-1]
+        child = next(children, None)
+        if child is None:
+            stack.pop()
+            visited.pop()
+        elif len(visited) < cutoff:
+            if child == target:
+                yield visited + [child]
+            elif child in visited:
+                continue
+            visited += [child]
+            if target not in visited:  # expand stack until find all targets
+                stack.append(iter(G[child]))
+            else:
+                visited.pop()  # maybe other ways to child
+        else:  # len(visited) == cutoff:
+            # for target in (targets & (set(children) | {child})) - set(visited.keys()):
+            yield visited + [target]
+            stack.pop()
+            visited.pop()
+
+
+def all_simple_edge_paths(G, source, target, cutoff=None):
+    """
+    Versione modificata di nx.all_simple_edge_paths.
+
+    """
+    if source not in G:
+        raise nx.NodeNotFound("source node %s not in graph" % source)
+    if target not in G:
+        raise nx.NodeNotFound("source node %s not in graph" % source)
+    # if source in targets:
+    #    return []
+    if cutoff is None:
+        cutoff = len(G) - 1
+    if cutoff < 1:
+        return []
+    return _all_simple_paths_graph(G, source, target)
+    # for simp_path in _all_simple_paths_graph(G, source, target):
+    #     yield list(zip(simp_path[:-1], simp_path[1:]))
--- extra_guglielmo/read_graph.py
+++ extra_guglielmo/read_graph.py
@@ -1,11 +1,10 @@
+from networkx.algorithms.bipartite.basic import color
 import pyqtgraph.opengl as gl
 import networkx as nx
 import pyqtgraph as pg
 import numpy as np
-import time
-from threading import Thread
-import datetime
 import argparse
+import graph_utils
 
 parser = argparse.ArgumentParser(
     description='Reduces vertices in OBJ-represented graph')
@@ -14,14 +13,15 @@ parser.add_argument('-p', '--plot', dest='plot',
                     action='store_true', help='Visualize the algorithm')
 global_options = parser.parse_args()
 
-pg.mkQApp()
-# make a widget for displaying 3D objects
-view = gl.GLViewWidget()
-lineWidget = gl.GLLinePlotItem()
-vertexWidget = gl.GLScatterPlotItem()
-view.addItem(lineWidget)
-view.addItem(vertexWidget)
-view.show()
+if global_options.plot:
+    pg.mkQApp()
+    # make a widget for displaying 3D objects
+    view = gl.GLViewWidget()
+    lineWidget = gl.GLLinePlotItem()
+    vertexWidget = gl.GLScatterPlotItem()
+    view.addItem(lineWidget)
+    view.addItem(vertexWidget)
+    view.show()
 
 FILE = 'curves.obj'
 
@@ -38,7 +38,7 @@ def graph_from_obj(objFileName: str):
     return graph
 
 
-def cleanup_graph(graph: nx.Graph, relabel_nodes=False):
+def cleanup_graph(graph: nx.Graph, relabel_nodes=False, visualize=global_options.plot):
     '''
         Removes every node with 2 adjacents (read "is not a triangle vertex in the mesh").
         If relabel_nodes is True, relabels the nodes with numbers from 1 to n
@@ -47,15 +47,16 @@ def cleanup_graph(graph: nx.Graph, relabel_nodes=False):
     stack = [next((n for n in graph.nodes if graph.degree(n) == 2), None)]
     if stack[0] == None:
         return
-    while len(stack):
+    while len(stack):  # TODO while stack
         node = stack.pop()
         adjacencents = list(graph.adj[node].keys())
         if not visited[node]:
             visited[node] = True
             stack += [n for n in adjacencents
                       if not visited[n] and n not in stack]
-            if global_options.plot:
-                plot_graph(graph, currentVertex=node, explored=visited)
+            if visualize:
+                plot_graph(graph, current_vertex=node,
+                           colored_vertices=[vis for vis in visited if visited[vis]])
             if graph.degree(node) == 2:
                 u, v = adjacencents
                 graph.remove_node(node)
@@ -81,21 +82,18 @@ def graph_to_obj(graph: nx.Graph, objFileName: str):
         destFile.writelines(lines)
 
 
-def plot_graph(graph: nx.Graph, currentVertex=None, explored=[], stack=[]):
-    vertices, vColors, vSizes, lines = [], [], [], []
+def plot_graph(graph: nx.Graph, current_vertex=None, colored_vertices=[], colored_edges=[]):
+    vertices, vColors, vSizes, lines, lColors = [], [], [], [], []
     for node in graph.nodes:
         vertices.append(np.array(
             graph.nodes(data=True)[node]['vector']
         ))
-        if node == currentVertex:
+        if node == current_vertex:
             vColors.append(np.array([0, 255, 0, 1]))
             vSizes.append(10)
-        elif len(explored) and explored[node]:
+        elif node in colored_vertices:
             vColors.append(np.array([0, 0, 255, 1]))
             vSizes.append(15)
-        elif node in stack:
-            vColors.append(np.array([255, 255, 0, 1]))
-            vSizes.append(10)
         else:
             vColors.append(np.array([255, 0, 0, 1]))
             vSizes.append(4)
@@ -106,13 +104,43 @@ def plot_graph(graph: nx.Graph, currentVertex=None, explored=[], stack=[]):
             np.array(graph.nodes(data=True)[u]['vector']))
         lines.append(
             np.array(graph.nodes(data=True)[v]['vector']))
-    lineWidget.setData(pos=np.array(lines), mode='lines')
+        if sorted(edge) in [sorted(col_edge) for col_edge in colored_edges]:
+            # , np.array([0, 255, 0, 1]))
+            lColors.append(np.array([255, 0, 0, 1]))
+            lColors.append(np.array([255, 0, 0, 1]))
+        else:
+            # ,np.array([255, 255, 255, 1]))
+            lColors.append(np.array([255, 255, 255, 1]))
+            lColors.append(np.array([255, 255, 255, 1]))
+    lineWidget.setData(pos=np.array(lines), mode='lines',
+                       color=np.array(lColors))
     vertexWidget.setData(pos=np.array(vertices),
                          color=np.array(vColors), size=np.array(vSizes))
     input("")
 
 
-graph = graph_from_obj('curves.obj')
-graph = cleanup_graph(graph, relabel_nodes=True)
+def reduce_to_triangles(graph: nx.Graph):
+    all_loops = {}
+    for node in graph.nodes:
+        all_loops[node] = list(
+            graph_utils.all_simple_edge_paths(graph, node, node))
+    return all_loops
 
-graph_to_obj(graph, 'out.obj')
+
+def plot_paths(graph: nx.Graph, paths: dict):
+    for node in paths:
+        print("\nNODE", node)
+        for path in paths[node]:
+            print(path)
+            print(list(zip(path[:-1], path[1:])))
+            print("\n")
+            plot_graph(graph, colored_vertices=path, current_vertex=node,
+                       colored_edges=list(zip(path[:-1], path[1:])))
+
+
+graph = graph_from_obj('curves.obj')
+graph = cleanup_graph(graph, relabel_nodes=True, visualize=False)
+paths = reduce_to_triangles(graph)
+plot_paths(graph, paths)
+# print(list(zip(paths[:-1], paths[1:])))
+# graph_to_obj(graph, 'out.obj')
