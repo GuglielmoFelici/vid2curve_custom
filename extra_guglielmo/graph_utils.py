c2e807ec9c36702554bec8aa2727517a64041e51
Guglielmo Felici <guglielmo.felici@protonmail.com>
2021-04-23 00:03:55 +0200

    pyqtgraph animation and cluster merging

--- extra_guglielmo/graph_utils.py
+++ extra_guglielmo/graph_utils.py
@@ -1,75 +1,144 @@
 import networkx as nx
+import numpy as np
+import visualize as plot
+# from visualize import plot_graph
 
 
-def _all_simple_paths_graph(G: nx.Graph, source, target):
-    current_path = [source]
-    # visited = dict.fromkeys(G.nodes, False)
-    stack = list(G[source])
-    while stack:
+def graph_from_obj(objFileName: str) -> nx.Graph:
+    graph = nx.Graph()
+    with open(objFileName, 'r') as objFile:
+        for idx, line in enumerate(objFile.readlines()):
+            line = line.split()
+            if line[0] == 'v':
+                graph.add_node(idx+1, vector=[float(coord)
+                                              for coord in line[1:4]])
+            elif line[0] == 'l':
+                graph.add_edge(int(line[1]), int(line[2]))
+    return graph
+
+
+def graph_to_obj(graph: nx.Graph, objFileName: str):
+    vertices = []
+    lines = []
+    graph = relabel_nodes(graph)
+    for node, node_vec in graph.nodes(data='vector'):
+        # TODO add color?
+        vertices.append(f'v {" ".join([str(coord) for coord in node_vec])}\n')
+    for edge in graph.edges:
+        u, v = list(edge)
+        lines.append(f'l {u} {v}\n')
+    with open(objFileName, 'w') as destFile:
+        destFile.writelines(vertices)
+        destFile.writelines(lines)
+
+
+def relabel_nodes(graph: nx.Graph) -> nx.Graph:
+    ''' Remaps node tags to numbers in [1, N] to ensure OBJ compatibility. Returns a new graph. '''
+    newGraph = nx.relabel_nodes(
+        graph,
+        {node: idx+1 for idx, node in enumerate(graph.nodes)}
+    )
+    return newGraph
+
+
+def path_to_edges(path: list) -> list:
+    ''' [1,2,3, 4] -> [(1,2), (2,3), (3,4)] '''
+    return list(zip(path[:-1], path[1:]))
+
+
+def vert_dist(u: list, v: list):
+    sqrd_dist = np.sum((np.array(u) - np.array(v))**2)
+    return np.sqrt(sqrd_dist)
+
+
+def get_vertex_pos(graph: nx.Graph, v: int):
+    return graph.nodes(data=True)[v]['vector']
+
+
+def simplify_edges(graph: nx.Graph, visualize=False):
+    '''
+        Removes every node with 2 adjacents (read "is not a triangle vertex in the mesh").
+    '''
+    visited = [False for i in range(graph.size())]
+    stack = [next((n for n in graph.nodes if graph.degree(n) == 2), None)]
+    if stack[0] == None:
+        return
+    while len(stack):  # TODO while stack
         node = stack.pop()
-        if node == None:
-            current_path.pop()
-            continue
-        elif node == target:
-            yield current_path + [node]
-            pass
-        if node in current_path:
-            continue
-        current_path += [node]
-        # elif node in current_path:
-        #     current_path.pop()
-        if G[node]:
-            stack.append(None)
-            stack += list(G[node])
-        else:
-            current_path.pop()
+        adjacencents = list(graph.adj[node].keys())
+        if not visited[node]:
+            visited[node] = True
+            stack += [n for n in adjacencents
+                      if not visited[n] and n not in stack]
+            if visualize:
+                plot.plot_graph(graph,
+                                highlighted_vertex=node,
+                                colored_vertices=[
+                                    vis for vis in graph.nodes if visited[vis]],
+                                title='Checking vertices for removal')
+            if graph.degree(node) == 2:
+                u, v = adjacencents
+                graph.remove_node(node)
+                graph.add_edge(u, v)
 
 
-def BAK_all_simple_paths_graph(G, source, target, cutoff):
-    """
-    Versione modificata di nx._all_simple_paths_graph per tornare simple paths che possano partire e arrivare allo stesso nodo
-    """
-    print(source, "\n")
-    visited = []  # [source]
-    stack = [iter(G[source])]
-    while stack:
-        children = stack[-1]
-        child = next(children, None)
-        if child is None:
-            stack.pop()
-            visited.pop()
-        elif len(visited) < cutoff:
-            if child == target:
-                yield visited + [child]
-            elif child in visited:
+def merge_close_vecs(graph: nx.Graph, visualize=False):
+    clusters = []
+    for u, u_vec in graph.nodes(data='vector'):
+        cluster = [u]
+        for v, v_vec in graph.nodes(data='vector'):
+            if u == v:
                 continue
-            visited += [child]
-            if target not in visited:  # expand stack until find all targets
-                stack.append(iter(G[child]))
-            else:
-                visited.pop()  # maybe other ways to child
-        else:  # len(visited) == cutoff:
-            # for target in (targets & (set(children) | {child})) - set(visited.keys()):
-            yield visited + [target]
-            stack.pop()
-            visited.pop()
+            sqrd_dist = np.sum((np.array(u_vec) - np.array(v_vec))**2)
+            dist = np.sqrt(sqrd_dist)
+            if (dist < 0.01):
+                cluster.append(v)
+                # to_merge.append((u, v))
+        if len(cluster) > 1:
+            clusters.append(cluster)
+    for cluster in clusters:
+        if set(cluster) - set(graph.nodes):
+            continue
+        if visualize:
+            plot.plot_graph(
+                graph,
+                colored_vertices=cluster,
+                frame_duration=1,
+                title='Cluster merging: vector cluster found')
+        centroid = np.divide(
+            np.sum(
+                [np.array(get_vertex_pos(graph, v)) for v in cluster],
+                axis=0),
+            len(cluster)
+        )
+        new_node = cluster[0]
+        new_adjacents = []
+        for node in cluster:
+            new_adjacents += list(graph.adj[node])
+        graph.remove_nodes_from(cluster)
+        graph.add_node(new_node, vector=centroid)
+        for adj in new_adjacents:
+            if graph.has_node(adj) and adj != new_node:
+                graph.add_edge(new_node, adj)
+        if visualize:
+            plot.plot_graph(
+                graph,
+                highlighted_vertex=new_node,
+                frame_duration=1,
+                title='Cluster merging: cluster merged in new vector')
+
+# def reduce_to_triangles(graph: nx.Graph):
+#     all_loops = {}
+#     for node in graph.nodes:
+#         all_loops[node] = list(
+#             graph_utils.all_simple_edge_paths(graph, node, node))
+#     return all_loops
 
 
-def all_simple_edge_paths(G, source, target, cutoff=None):
-    """
-    Versione modificata di nx.all_simple_edge_paths.
-
-    """
-    if source not in G:
-        raise nx.NodeNotFound("source node %s not in graph" % source)
-    if target not in G:
-        raise nx.NodeNotFound("source node %s not in graph" % source)
-    # if source in targets:
-    #    return []
-    if cutoff is None:
-        cutoff = len(G) - 1
-    if cutoff < 1:
-        return []
-    return _all_simple_paths_graph(G, source, target)
-    # for simp_path in _all_simple_paths_graph(G, source, target):
-    #     yield list(zip(simp_path[:-1], simp_path[1:]))
+# def path_is_face(graph: nx.Graph, path: list):
+#     edges = [sorted(edge) for edge in path_to_edges(path)]
+#     for node in path:
+#         for adj in list(graph[node]):
+#             if adj in path and sorted((node, adj)) not in edges:
+#                 return False
+#     return True
---
+++ extra_guglielmo/visualize.py
@@ -0,0 +1,88 @@
+import networkx as nx
+import pyqtgraph.opengl as gl
+import pyqtgraph as pg
+import numpy as np
+from graph_utils import get_vertex_pos, path_to_edges
+from PyQt5 import QtCore
+import os
+import time
+
+
+view = lineWidget = vertexWidget = None
+
+
+def init_window():
+    global view, lineWidget, vertexWidget
+    pg.mkQApp()
+    # make a widget for displaying 3D objects
+    view = gl.GLViewWidget()
+    lineWidget = gl.GLLinePlotItem()
+    vertexWidget = gl.GLScatterPlotItem()
+    view.addItem(lineWidget)
+    view.addItem(vertexWidget)
+    view.show()
+
+
+def _plot_graph(graph: nx.Graph, highlighted_vertex=None, colored_vertices=[], colored_edges=[], title='', wait_for_input=True, skip_key='q', clear_console=True) -> bool:
+    QtCore.QTimer.singleShot(20000, lambda: print("heyo"))
+    print("heyo")
+    return True
+
+
+def responsive_sleep(secs: int):
+    i = 0
+    while i < secs:
+        time.sleep(0.01)
+        i += 0.01
+        pg.mkQApp().processEvents()
+
+
+def plot_graph(graph: nx.Graph, highlighted_vertex=None, colored_vertices=[], colored_edges=[], title='', is_frame=True, frame_duration=0.01):
+    ''' Plots the graph. If is_frame is True, pauses the program for frame_duration (to be used in loops).'''
+
+    if not view or not lineWidget or not vertexWidget:
+        return False
+    view.setWindowTitle(title)
+    vertices, vColors, vSizes, lines, lColors = [], [], [], [], []
+    for node, node_vec in graph.nodes(data='vector'):
+        vertices.append(np.array(
+            node_vec
+        ))
+        if node == highlighted_vertex:
+            vColors.append(np.array([0, 255, 0, 1]))
+            vSizes.append(10)
+        elif node in colored_vertices:
+            vColors.append(np.array([0, 0, 255, 1]))
+            vSizes.append(15)
+        else:
+            vColors.append(np.array([255, 0, 0, 1]))
+            vSizes.append(4)
+    lines = []
+    for edge in graph.edges:
+        u, v = list(edge)
+        lines.append(
+            np.array(get_vertex_pos(graph, u)))
+        lines.append(
+            np.array(get_vertex_pos(graph, v)))
+        if sorted(edge) in [sorted(col_edge) for col_edge in colored_edges]:
+            # , np.array([0, 255, 0, 1]))
+            lColors.append(np.array([255, 0, 0, 1]))
+            lColors.append(np.array([255, 0, 0, 1]))
+        else:
+            # ,np.array([255, 255, 255, 1]))
+            lColors.append(np.array([255, 255, 255, 1]))
+            lColors.append(np.array([255, 255, 255, 1]))
+    lineWidget.setData(pos=np.array(lines), mode='lines',
+                       color=np.array(lColors))
+    vertexWidget.setData(pos=np.array(vertices),
+                         color=np.array(vColors), size=np.array(vSizes))
+    if is_frame:
+        frame_duration = max(frame_duration, min(frame_duration, 0.01))
+        responsive_sleep(frame_duration)
+
+
+def plot_cycles(graph: nx.Graph, paths: dict):
+    for node in paths:
+        for path in paths[node]:
+            plot_graph(graph, colored_vertices=path, highlighted_vertex=node,
+                       colored_edges=path_to_edges(path))
